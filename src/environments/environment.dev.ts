ea039114a2fd29960d705840dc98de483f1428ce
seiji142 <seijitsumura1985@gmail.com>
2026-03-06 16:25:14 -0300

    configuracion proyecto angular

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-   //url: 'http://api-rifas.dev.detoqueytoque.com',// NO SEGURO
-   url: 'https://api-rifas.dev.detoqueytoque.com',
+   //url: 'https://api-rifas.dev.detoqueytoque.com',
+   url:'https://api-rifas.ct.ws',
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  url: 'https://apirifas.detoqueytoque.com',
+  //url: 'https://apirifas.detoqueytoque.com',
+  url:'https://api-rifas.ct.ws',
 }
